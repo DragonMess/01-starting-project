f6d1f8cb9c105225c7d42924da7eb9c62a06ece3
DragonMess <kmylop@hotmail.com>
2022-02-06 10:44:38 -0500

    added useEffect

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    const alreadyLogged = localStorage.getItem("logged");
+    if (alreadyLogged === "1") {
+      setIsLoggedIn(true);
+    }
+  }, [isLoggedIn]);
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem("logged", "1");
     setIsLoggedIn(true);
+    console.log(isLoggedIn);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem("logged", "1");
     setIsLoggedIn(false);
   };
 
